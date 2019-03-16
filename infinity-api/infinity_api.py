156dfb19eff8fa6ab44940620d192807d51d70d5
Carlos Gomez <kurolox@mailbox.org>
2019-03-16 16:17:16 +0100

    Added base for flask-restplus file

--- infinity-api/infinity_api.py
+++ infinity-api/infinity_api.py
@@ -1,13 +1,34 @@
-from flask import request, url_for
-from flask_api import FlaskAPI, status, exceptions
-
-app = FlaskAPI(__name__)
-
-
-@app.route("/test/", methods=['GET'])
-def notes_detail():
-    return {'request data': request.data}
+from flask import Flask
+from flask_restplus import Resource, Api
+from playhouse.shortcuts import model_to_dict
+from db_classes import db, Unit, Weapon, Ammo, Ability, Characteristic, \
+    Sectorial, Profile, String, WeaponProperty, Property, ProfileWeapon, \
+    ProfileCharacteristic, ProfileAbility, UnitCharacteristic, \
+    UnitAbility
 
 
-if __name__ == "__main__":
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/unit/<int:unit_id>')
+class ReturnUnit(Resource):
+    def get(self, unit_id):
+        try:
+            unit = Unit[unit_id]
+        except Unit.DoesNotExist:
+            return "Unit not found", 404
+        return model_to_dict(Unit[unit_id])
+
+
+@api.route('/unit')
+class ReturnAllUnits(Resource):
+    def get(self):
+        units = {}
+        for unit in Unit.select():
+            units[unit.unit_id] = model_to_dict(unit)
+        return units
+
+
+if __name__ == '__main__':
     app.run()
